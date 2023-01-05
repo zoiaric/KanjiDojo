1e775f3c9bac2d55d8d038da2de4858b1755e052
Riccardo Zoia <riccardozoia@Riccardos-MBP.homenet.telecomitalia.it>
2023-01-05 17:08:44 +0100

    a

---
+++ KanjiDojo/Views/Instructions.swift
@@ -0,0 +1,20 @@
+//
+//  Instructions.swift
+//  KanjiDojo
+//
+//  Created by Riccardo Zoia on 06/12/22.
+//
+
+import SwiftUI
+
+struct Instructions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Instructions_Previews: PreviewProvider {
+    static var previews: some View {
+        Instructions()
+    }
+}
---
+++ KanjiDojo/Views/SupportMe.swift
@@ -0,0 +1,20 @@
+//
+//  SupportMe.swift
+//  KanjiDojo
+//
+//  Created by Riccardo Zoia on 06/12/22.
+//
+
+import SwiftUI
+
+struct SupportMe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SupportMe_Previews: PreviewProvider {
+    static var previews: some View {
+        SupportMe()
+    }
+}
